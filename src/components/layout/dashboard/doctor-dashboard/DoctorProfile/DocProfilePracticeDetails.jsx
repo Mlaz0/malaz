9dfbd2ab9ea62ce17ddb184a6234d5980ce279f8
Ahmed Assem <ahmedassem2610@gmail.com>
2025-07-25 16:48:50 +0300

    availability form refactor

--- src/components/layout/dashboard/doctor-dashboard/DoctorProfile/DocProfilePracticeDetails.jsx
+++ src/components/layout/dashboard/doctor-dashboard/DoctorProfile/DocProfilePracticeDetails.jsx
@@ -1,7 +1,7 @@
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { Clock, DollarSign, Calendar } from "lucide-react";
+import { Clock, Calendar } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { formatDate, formatPrice, formatTime } from "@/utils/formatOperations";
